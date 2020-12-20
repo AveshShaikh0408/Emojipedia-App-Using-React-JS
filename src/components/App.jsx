a6c74782d4467abe82fcc63f41ef9ba63222d5ac
Avesh Shaikh <31967343+AveshShaikh0408@users.noreply.github.com>
2020-12-20 21:03:56 +0530

    Updated App.JS Updated App.js file with ES6 arrow function for Card.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,24 +2,22 @@ import React from "react";
 import Card from "./Card";
 import emojiData from "./emojiData";
 
-function createEntry(emojiData) {
-  return (
-    <Card
-      key={emojiData.id}
-      emoji={emojiData.emoji}
-      name={emojiData.name}
-      meaning={emojiData.meaning}
-    />
-  );
-}
-
 function App() {
   return (
     <div>
       <h1>
         <span>emojipedia</span>
       </h1>
-      <dl className="dictionary">{emojiData.map(createEntry)}</dl>
+      <dl className="dictionary">
+        {emojiData.map((emojiData) => (
+          <Card
+            key={emojiData.id}
+            emoji={emojiData.emoji}
+            name={emojiData.name}
+            meaning={emojiData.meaning}
+          />
+        ))}
+      </dl>
     </div>
   );
 }
